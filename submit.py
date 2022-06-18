cb26e214138127ebbf07016b4ae5fe9e32b95e95
Lanbin <huoziye123@163.com>
2022-06-18 20:41:38 +0800

    2022.06.18 20:41 | Update docs

---
+++ submit.py
@@ -0,0 +1,16 @@
+from os import system
+import time
+import datetime
+import locale
+
+a = input("Description (提交描述): >>")
+
+locale.setlocale(locale.LC_CTYPE, 'Chinese')
+
+debug = False
+date_time = datetime.datetime.now().strftime("%Y.%m.%d %H:%M | "+a)
+
+if not debug:
+    system("git add .")
+    system("git commit -m \""+ date_time +"\"")
+    system("git push")
